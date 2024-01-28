1eeb7766102ae62edbf1b2572143e3e0ac6cc80c
Volodymyr <vladikon70@gmail.com>
2024-01-28 12:20:15 +0100

    test project

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -4,6 +4,8 @@ import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'https://watertracker-fzxp.onrender.com';
 
+console.log(axios.defaults.baseURL);
+
 export const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
